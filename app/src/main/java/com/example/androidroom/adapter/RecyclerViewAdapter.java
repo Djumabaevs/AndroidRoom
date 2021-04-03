c0695d788eea6db4b2a93e19229d256f3d9d2b03
Bakyt <djumabaevb@gmail.com>
2021-04-03 11:26:54 +0600

    constructing view holder

--- app/src/main/java/com/example/androidroom/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/androidroom/adapter/RecyclerViewAdapter.java
@@ -1,17 +1,36 @@
 package com.example.androidroom.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.androidroom.R;
+import com.example.androidroom.model.Contact;
+
+import java.util.List;
+
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private LiveData<List<Contact>> contactList;
+    private Context context;
+
+    public RecyclerViewAdapter(LiveData<List<Contact>> contactList, Context context) {
+        this.contactList = contactList;
+        this.context = context;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.contact_row, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
@@ -21,13 +40,18 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public int getItemCount() {
-        return 0;
+        return contactList.getValue().size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        public TextView name;
+        public TextView occupation;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            name = itemView.findViewById(R.id.row_name_textview);
+            occupation = itemView.findViewById(R.id.row_occupation_textview);
         }
     }
 }
